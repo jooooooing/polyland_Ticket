948c5dfc2e18f83756c19415c1304e62a3164ee9
jooooooing <leegun1018@naver.com>
2022-03-21 13:02:15 +0900

    입력값_반복추가

--- source/polyland_ticket.c
+++ source/polyland_ticket.c
@@ -14,10 +14,10 @@ int main(){
 	int resultPrice, premierPrice;
 	int ageNumber, birthYear, birthDate, seventhDate, yearGap, interAge;
 	int thisYear = t->tm_year + 1900, thisDate=316;
-	const int full_1DayPriceAdult = 59000, full_1DayPriceTeen = 52000, full_1DayPriceKids = 47000, full_1DayPriceBaby = 15000;
-	const int full_After4PriceAdult = 48000, full_After4PriceTeen = 42000, full_After4PriceKids = 36000, full_After4PriceBaby = 15000;
-	const int park_1DayPriceAdult = 56000, park_1DayPriceTeen = 50000, park_1DayPriceKids = 46000, park_1DayPriceBaby = 15000;
-	const int park_After4PriceAdult = 45000, park_After4PriceTeen = 40000, park_After4PriceKids = 35000, park_After4PriceBaby = 15000;
+	const int FULL_1DAYPRICEADULT = 59000, FULL_1DAYPRICETEEN = 52000, FULL_1DAYPRICEKIDS = 47000, FULL_1DAYPRICEBABY = 15000;
+	const int FULL_AFTER4PRICEADULT = 48000, FULL_AFTER4PRICETEEN = 42000, FULL_AFTER4PRICEKIDS = 36000, FULL_AFTER4PRICEBABY = 15000;
+	const int PARK_1DAYPRICEADULT = 56000, PARK_1DAYPRICETEEN = 50000, PARK_1DAYPRICEKIDS = 46000, PARK_1DAYPRICEBABY = 15000;
+	const int PARK_AFTER4PRICEADULT = 45000, PARK_AFTER4PRICETEEN = 40000, PARK_AFTER4PRICEKIDS = 35000, PARK_AFTER4PRICEBABY = 15000;
 	char todayMonth, todayDay, today;
 	
 	todayMonth = t->tm_mon+1;
@@ -27,22 +27,27 @@ int main(){
 	printf("%d\n", thisYear);
 	printf("%d %d %d\n", todayMonth, todayDay, today);
 	
-	printf("티켓 종류를 입력하세요. \n 1. 종합이용권 \n 2. 파크이용권\n");
-	scanf("%d", &inputTicket);
-	
-	if (inputTicket ==1 || inputTicket == 2) {  //잘못 입력시 반복 넣기위함// 
-	
-	printf("세부 티켓 종류를 입력하세요. \n 1. 1Day \n 2. After4\n");
-	scanf("%d", &inputTicketDetail);	
 		
-	printf("주민번호를 입력하세요(7자리까지)\n");
-	scanf("%d", &inputBirth);
+	do {																			//입력 반복// 
+		printf("티켓 종류를 입력하세요. \n 1. 종합이용권 \n 2. 파크이용권\n");
+		
+		scanf("%d", &inputTicket);
 	
-	printf("몇개를 주문하시겠습니까? (최대 10개)\n");
-	scanf("%d", &inputAmount);
+		printf("세부 티켓 종류를 입력하세요. \n 1. 1Day \n 2. After4\n");
+		scanf("%d", &inputTicketDetail);	
+		
+		printf("주민번호를 입력하세요(7자리까지)\n");
+		scanf("%d", &inputBirth);
 	
-	printf("우대 사항을 선택하세요. \n 1. 없음(나이 우대는 자동처리) \n 2. 장애인\n 3. 국가유공자\n 4. 휴가장병\n 5. 임산부\n 6. 다둥이 행복카드\n");
-	scanf("%d", &inputPremier);
+		printf("몇개를 주문하시겠습니까? (최대 10개)\n");
+		scanf("%d", &inputAmount);
+	
+		printf("우대 사항을 선택하세요. \n 1. 없음(나이 우대는 자동처리) \n 2. 장애인\n 3. 국가유공자\n 4. 휴가장병\n 5. 임산부\n 6. 다둥이 행복카드\n");
+		scanf("%d", &inputPremier);
+		
+		if ( inputTicket > 2 || inputTicketDetail > 2 || inputBirth%10 > 5 || inputAmount > 10 || inputPremier > 7 )
+		printf ("입력이 잘못되었습니다. 다시 입력해주세요\n\n");
+	} while ( inputTicket > 2 || inputTicketDetail > 2 || inputBirth%10 > 5 || inputAmount > 10 || inputPremier > 7 );
 	
 	//나이 계산// 
 	ageNumber = inputBirth/100000; //주민번호 7자리를 10만으로 나누어 생년 정수형으로 변환 
@@ -75,114 +80,130 @@ int main(){
 	// --- //
 	
 	// 티켓 값 계산 //
-	// 만 65세 이상 // 
 	
-	if (interAge >= 65 && inputTicket == 1 && inputTicketDetail == 1) {
-		resultPrice = full_1DayPriceKids * inputAmount;
-	}
-	
-	else if (interAge >= 65 && inputTicket == 1 && inputTicketDetail == 2) {
-		resultPrice = full_After4PriceKids * inputAmount;
-	}
-	
-	else if (interAge >= 65 && inputTicket == 2 && inputTicketDetail == 1) {
-		resultPrice = park_1DayPriceKids * inputAmount;
-	}
-	
-	else if (interAge >= 65 && inputTicket == 2 && inputTicketDetail == 2) {
-		resultPrice = park_After4PriceKids * inputAmount;
+	// 만 65세 이상 // 	
+	if (interAge >= 65)	{
+		if(inputTicket == 1) {   											//종일 이용권 선택 // 
+			if (inputTicketDetail ==1 ){  									// 1 Day //
+			resultPrice = FULL_1DAYPRICEKIDS * inputAmount;
+			}
+			else  if (inputTicketDetail ==2 ){ 								// After4 //
+			resultPrice = FULL_AFTER4PRICEKIDS * inputAmount;
+			}
+		else (inputTicket == 2) ;											//파크  이용권 선택 // 
+			if (inputTicketDetail ==1 ){ 									// 1 Day //
+			resultPrice = PARK_1DAYPRICEKIDS * inputAmount;
+			}
+			else  if (inputTicketDetail ==2 ){ 								// After4 //
+			resultPrice = PARK_AFTER4PRICEKIDS * inputAmount;
+			}
+		}	
 	}
 	
 	// 어른 만 19세 이상  ~ 만 65세 미만//
 	
-	if (interAge >= 19 && interAge < 65 && inputTicket == 1 && inputTicketDetail == 1) {
-		resultPrice = full_1DayPriceAdult * inputAmount;
+	else if (interAge >= 19){
+		if(inputTicket == 1) {   											//종일 이용권 선택 // 
+			if (inputTicketDetail ==1 ){  									// 1 Day //
+			resultPrice = FULL_1DAYPRICEADULT * inputAmount;
+			}
+			else  if (inputTicketDetail ==2 ){ 								// After4 //
+			resultPrice = FULL_AFTER4PRICEADULT * inputAmount;
+			}
+		else (inputTicket == 2) ;											//파크  이용권 선택 // 
+			if (inputTicketDetail ==1 ){ 									// 1 Day //
+			resultPrice = PARK_1DAYPRICEADULT * inputAmount;
+			}
+			else  if (inputTicketDetail ==2 ){ 								// After4 //
+			resultPrice = PARK_AFTER4PRICEADULT * inputAmount;
+			}
+		}
 	}
-	
-	else if (interAge >= 19 && interAge < 65 && inputTicket == 1 && inputTicketDetail == 2) {
-		resultPrice = full_After4PriceAdult * inputAmount;
-	}
-	
-	else if (interAge >= 19 && interAge < 65 && inputTicket == 2 && inputTicketDetail == 1) {
-		resultPrice = park_1DayPriceAdult * inputAmount;
-	}
-	
-	else if (interAge >= 19 && interAge < 65 && inputTicket == 2 && inputTicketDetail == 2) {
-		resultPrice = park_After4PriceAdult * inputAmount;
-	}
-	 
+		
 	// 청소년 만 13세 이상 ~ 만 18세 이하 //
 	
-	if (interAge >= 13 && interAge <= 18 && inputTicket == 1 && inputTicketDetail == 1) {
-		resultPrice = full_1DayPriceTeen * inputAmount;
+	else if (interAge >= 13){
+		if(inputTicket == 1) {   											//종일 이용권 선택 // 
+			if (inputTicketDetail ==1 ){  									// 1 Day //
+			resultPrice = FULL_1DAYPRICETEEN * inputAmount;
+			}
+			else  if (inputTicketDetail ==2 ){ 								// After4 //
+			resultPrice = FULL_AFTER4PRICETEEN * inputAmount;
+			}
+		else (inputTicket == 2) ;											//파크  이용권 선택 // 
+			if (inputTicketDetail ==1 ){ 									// 1 Day //
+			resultPrice = PARK_1DAYPRICETEEN * inputAmount;
+			}
+			else  if (inputTicketDetail ==2 ){ 								// After4 //
+			resultPrice = PARK_AFTER4PRICETEEN * inputAmount;
+			}
+		}
 	}
-	
-	else if (interAge >= 13 && interAge <= 18 && inputTicket == 1 && inputTicketDetail == 2) {
-		resultPrice = full_After4PriceTeen * inputAmount;
-	}
-	
-	else if (interAge >= 13 && interAge <= 18 && inputTicket == 2 && inputTicketDetail == 1) {
-		resultPrice = park_1DayPriceTeen * inputAmount;
-	}
-	
-	else if (interAge >= 13 && interAge <= 18 && inputTicket == 2 && inputTicketDetail == 2) {
-		resultPrice = park_After4PriceTeen * inputAmount;
-	} 
+		
 	
 	// 어린이 36개월(만 3세) 이상 ~ 만 12세 이하 //
 	
-	if (interAge >= 3 && interAge <= 12 && inputTicket == 1 && inputTicketDetail == 1) {
-		resultPrice = full_1DayPriceKids * inputAmount;
-	}
+	else if (interAge >= 3){
+		if(inputTicket == 1) {   											//종일 이용권 선택 // 
+			if (inputTicketDetail ==1 ){  									// 1 Day //
+			resultPrice = FULL_1DAYPRICEKIDS * inputAmount;
+			}
+			else  if (inputTicketDetail ==2 ){ 								// After4 //
+			resultPrice = FULL_AFTER4PRICEKIDS * inputAmount;
+			}
+		else (inputTicket == 2) ;											//파크  이용권 선택 // 
+			if (inputTicketDetail ==1 ){ 									// 1 Day //
+			resultPrice = PARK_1DAYPRICEKIDS * inputAmount;
+			}
+			else  if (inputTicketDetail ==2 ){ 								// After4 //
+			resultPrice = PARK_AFTER4PRICEKIDS * inputAmount;
+			}
+		}
+	}		
+
+
+	// 베이비 0개월 이상 ~ 36개월 (만3세) 미만 //
 	
-	else if (interAge >= 3 && interAge <= 12 && inputTicket == 1 && inputTicketDetail == 2) {
-		resultPrice = full_After4PriceKids * inputAmount;
+	else if (interAge < 3){
+		if(inputTicket == 1) {   											//종일 이용권 선택 // 
+			if (inputTicketDetail ==1 ){  									// 1 Day //
+			resultPrice = FULL_1DAYPRICEBABY * inputAmount;
+			}
+			else  if (inputTicketDetail ==2 ){ 								// After4 //
+			resultPrice = FULL_AFTER4PRICEBABY * inputAmount;
+			}
+		else (inputTicket == 2) ;											//파크  이용권 선택 // 
+			if (inputTicketDetail ==1 ){ 									// 1 Day //
+			resultPrice = PARK_1DAYPRICEBABY * inputAmount;
+			}
+			else  if (inputTicketDetail ==2 ){ 								// After4 //
+			resultPrice = PARK_AFTER4PRICEBABY * inputAmount;
+			}
+		}
 	}
-	
-	else if (interAge >= 3 && interAge <= 12 && inputTicket == 2 && inputTicketDetail == 1) {
-		resultPrice = park_1DayPriceKids * inputAmount;
-	}
-	
-	else if (interAge >= 3 && interAge <= 12 && inputTicket == 2 && inputTicketDetail == 2) {
-		resultPrice = park_After4PriceKids * inputAmount;
-	} 
-	
-		// 베이비 0개월 이상 ~ 36개월 (만3세) 미만 //
-	
-	if (interAge < 3 && inputTicket == 1 && inputTicketDetail == 1) {
-		resultPrice = full_1DayPriceBaby * inputAmount;
-	}
-	
-	else if (interAge < 3 && inputTicket == 1 && inputTicketDetail == 2) {
-		resultPrice = full_1DayPriceBaby * inputAmount;
-	}
-	
-	else if (interAge < 3 && inputTicket == 2 && inputTicketDetail == 1) {
-		resultPrice = full_1DayPriceBaby * inputAmount;
-	}
-	else if (interAge < 3 && inputTicket == 2 && inputTicketDetail == 2) {
-		resultPrice = full_1DayPriceBaby * inputAmount;
-	}
+
+
 
 	// 우대 요금 적용 *동반1인은 미적용 //
 	
 	printf ("선택한 티켓 종류는 : %d\n", inputTicket);
 	printf ("선택한 세부티켓 종류는 : %d\n", inputTicketDetail);
 	printf ("나이 구분 : %d\n", interAge); 
-	printf("주문 갯수 (최대 10개)\n", &inputAmount);
+	printf("주문 갯수 (최대 10개) : %d\n", inputAmount);
 	
-	//만 65세 이상 우대 적용 불가// 
+	//만 65세 이상 추가 우대 적용 불가// 
 	if (interAge >= 65) {
 		printf("65세 이상으로 베이비요금 적용\n");
-		printf("가격은 %d원 입니다.", resultPrice); 
+		printf("가격은 %d원 입니다.", resultPrice);	
 	}
 	
 	//우대 사항 없음// 
 	else if (inputPremier == 1) {
-		printf("우대사항 없음\n");
-		printf("가격은 %d원 입니다.", resultPrice); 
+		premierPrice = resultPrice * 0.5;
+		printf("장애인 우대 50%% \n");
+		printf("가격은 %d원 입니다.", premierPrice);
 	}
-	
+			
 	//장애인 우대 50% // 
 	else if (inputPremier == 2) {
 		premierPrice = resultPrice * 0.5;
@@ -217,13 +238,7 @@ int main(){
 		printf("다둥이 행복카드 본인 30%% \n");
 		printf("가격은 %d원 입니다.", premierPrice);
 	}
-}
-	
-	else {
-	printf ("다시 입력하세요\n");
-	}
 	
 	return 0;
 
-	
 }
